231c61729ff36409c742949e45954739a99e448b
Pablo Correa <juanpabloc2000@gmail.com>
2020-10-29 12:02:10 -0500

    moviment 0.1f

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+    private float direction;
+    private Rigidbody2D rgbd;
+
+    private void Awake()
+    {
+        rgbd = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void KeyboardMoviment()
+    {
+        direction = Input.GetAxis("Horizontal");
+        direction = direction * velocity * Time.deltaTime;
+
+        rgbd.velocity += new Vector2(direction, 0);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        KeyboardMoviment();
+    }
+}
